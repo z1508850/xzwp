6351957e907e72206926c7fa9403c0e10216ec3a
z1508850 <73526298+z1508850@users.noreply.github.com>
2023-09-14 03:42:06 +0800

    Add files via upload

---
+++ js/time.js
@@ -0,0 +1,32 @@
+function siteTime(){
+        window.setTimeout("siteTime()", 1000);
+        var seconds = 1000;
+        var minutes = seconds * 60;
+        var hours = minutes * 60;
+        var days = hours * 24;
+        var years = days * 365;
+        var today = new Date();
+        var todayYear = today.getFullYear();
+        var todayMonth = today.getMonth()+1;
+        var todayDate = today.getDate();
+        var todayHour = today.getHours();
+        var todayMinute = today.getMinutes();
+        var todaySecond = today.getSeconds();
+        var t1 = Date.UTC(2023,09,14,03,17,00);
+        var t2 = Date.UTC(todayYear,todayMonth,todayDate,todayHour,todayMinute,todaySecond);
+        var diff = t2-t1;
+        var diffYears = Math.floor(diff/years);
+        var diffDays = Math.floor((diff/days)-diffYears*365);
+        var diffHours = Math.floor((diff-(diffYears*365+diffDays)*days)/hours);
+        var diffMinutes = Math.floor((diff-(diffYears*365+diffDays)*days-diffHours*hours)/minutes);
+        var diffSeconds = Math.floor((diff-(diffYears*365+diffDays)*days-diffHours*hours-diffMinutes*minutes)/seconds);
+        if (diffMinutes < 60 && diffHours < 1 && diffDays < 1) {
+        document.getElementById("sitetime").innerHTML=+diffMinutes+"分钟前";
+        }else if (diffHours < 24 && diffDays < 1) {
+            document.getElementById("sitetime").innerHTML=+diffHours+"小时前";
+        }else{
+            document.getElementById("sitetime").innerHTML=+diffDays+"天前";
+        }
+        
+    }
+    siteTime(); 
