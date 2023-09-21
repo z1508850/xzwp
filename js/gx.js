9ec901bc5e53797c5ad41d5eab0f77e840450156
z1508850 <73526298+z1508850@users.noreply.github.com>
2023-09-21 11:44:24 +0800

    Update gx.js

--- js/gx.js
+++ js/gx.js
@@ -1,13 +1,13 @@
-function gx2(){iziToast.info({timeout:15000,icon:"Fontawesome",closeOnEscape:"true",transitionOut:"fadeOutRight",displayMode:"replace",layout:"2",transitionIn:"bounceInLeft",position:"topRight",icon:"fa fa-check-circle",backgroundColor:"#fff",title:"添加天卡OMG网盘",message:"自带BOT 支持NE BGX"})}
+function gx2(){iziToast.info({timeout:16000,icon:"Fontawesome",closeOnEscape:"true",transitionOut:"fadeOutRight",displayMode:"replace",layout:"2",transitionIn:"bounceInLeft",position:"topRight",icon:"fa fa-check-circle",backgroundColor:"#fff",title:"添加天卡OMG网盘",message:"自带BOT 支持NE BGX"})}
 
 
 
 
-function gx1(){iziToast.info({timeout:16000,icon:"Fontawesome",closeOnEscape:"true",transitionOut:"fadeOutRight",displayMode:"replace",layout:"2",transitionIn:"bounceInLeft",position:"topRight",icon:"fa fa-check-circle",backgroundColor:"#fff",title:"添加周卡暗夜网盘",message:"自带BOT 支持NE BGX"})}
+function gx1(){iziToast.info({timeout:17000,icon:"Fontawesome",closeOnEscape:"true",transitionOut:"fadeOutRight",displayMode:"replace",layout:"2",transitionIn:"bounceInLeft",position:"topRight",icon:"fa fa-check-circle",backgroundColor:"#fff",title:"添加周卡暗夜网盘",message:"自带BOT 支持NE BGX"})}
 function gx4(){iziToast.info({timeout:16000,icon:"Fontawesome",closeOnEscape:"true",transitionOut:"fadeOutRight",displayMode:"replace",layout:"2",transitionIn:"bounceInLeft",position:"topRight",icon:"fa fa-arrow-circle-up",backgroundColor:"#fff",title:"更新周卡SKY地址",message:"自带BOT 支持NE BGX"})}
 
 
-function gx3(){iziToast.info({timeout:14000,icon:"Fontawesome",closeOnEscape:"true",transitionOut:"fadeOutRight",displayMode:"replace",layout:"2",transitionIn:"bounceInLeft",position:"topRight",icon:"fa-solid fa-circle-info",backgroundColor:"#fff",title:"更新日志",message:"一分钟内不再提示"})}
+function gx3(){iziToast.info({timeout:15000,icon:"Fontawesome",closeOnEscape:"true",transitionOut:"fadeOutRight",displayMode:"replace",layout:"2",transitionIn:"bounceInLeft",position:"topRight",icon:"fa-solid fa-circle-info",backgroundColor:"#fff",title:"更新日志",message:"一分钟内不再提示"})}
 
    function setCookie(name, value, expire) {    
                   window.document.cookie = name + "=" + escape(value) + ((expire == null) ? "" : ("; expires=" + expire.toGMTString())); 
@@ -42,10 +42,10 @@ function gx3(){iziToast.info({timeout:14000,icon:"Fontawesome",closeOnEscape:"tr
                   }
                   register();
                    
-                 setTimeout(gx4 , 300);
-                 setTimeout(gx1 , 1300);
-                 setTimeout(gx2 , 2300); 
-                 setTimeout(gx3 , 3300);    
+                 setTimeout(gx4 , 500);
+                 setTimeout(gx1 , 1500);
+                 setTimeout(gx2 , 2500); 
+                 setTimeout(gx3 , 3500);    
               
                    
                 }
