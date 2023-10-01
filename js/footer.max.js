1bea70f94ffa1e1cabe459cdf7f48b5d29c6a1e9
z1508850 <73526298+z1508850@users.noreply.github.com>
2023-10-02 03:02:59 +0800

    Update footer.max.js

--- js/footer.max.js
+++ js/footer.max.js
@@ -23,7 +23,34 @@ var a = 'retrtrfdcfvvvv';
   }
   if (ym3.indexOf(ym2) == -1 ) {
 if (is_weixn_qq()) {;
+
+ var os = function (){
+    var ua = navigator.userAgent,
+    isWindowsPhone = /(?:Windows Phone)/.test(ua),
+    isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
+    isAndroid = /(?:Android)/.test(ua),
+    isFireFox = /(?:Firefox)/.test(ua),
+    isChrome = /(?:Chrome|CriOS)/.test(ua),
+    isTablet = /(?:iPad|PlayBook)/.test(ua) || (isAndroid && !/(?:Mobile)/.test(ua)) || (isFireFox && /(?:Tablet)/.test(ua)),
+    isPhone = /(?:iPhone)/.test(ua) && !isTablet,
+    isPc = !isPhone && !isAndroid && !isSymbian;
+    return {
+        isTablet: isTablet,
+        isPhone: isPhone,
+        isAndroid: isAndroid,
+        isPc: isPc
+    };
+}();
+
+if (os.isAndroid || os.isPhone) {
 window.location.href = 'https://qimdav449ao.feishu.cn/docx/HF0KdvvQ1oIVRRx4SfNc2DAFn2e';
+} else if (os.isTablet) {
+window.location.href = 'https://qimdav449ao.feishu.cn/docx/HF0KdvvQ1oIVRRx4SfNc2DAFn2e';
+} else if(os.isPc) {
+
+}
+
+
 }
 function is_weixn_qq(){
 var ua = navigator.userAgent.toLowerCase();
