11c91baf44dfefabb6e73ee010377c8a26844588
z1508850 <73526298+z1508850@users.noreply.github.com>
2023-11-01 22:58:21 +0800

    Update footer.max.js

--- js/footer.max.js
+++ js/footer.max.js
@@ -1,3 +1,73 @@
+
+const style = document.createElement('style');
+  style.innerText = `
+    .barrage {
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: #ffffffe6;
+      color: #333;
+      padding: 10px 15px;
+      border-radius: 5px;
+      z-index: 9999;
+      display: flex;
+      align-items: center;
+    }
+
+    .barrage-icon {
+      display: block;
+      width: 20px;
+      height: 20px;
+      background-image: url('https://cdn3.codesign.qq.com/screens/previews/2023/10/24/jM93UEfMyMIRXfy8jBGwHATwiF0mBZ3ngnmn0qcd.png'); 
+      background-size: cover;
+      margin-right: 5px;
+      margin-left: -5px;
+      line-height: 20px;
+    }
+  `;
+  document.head.appendChild(style);
+
+  function showBarrage(event) {
+    if (window.innerWidth < 768) {
+      return;
+    }
+
+    event.preventDefault(); 
+
+    const barrage = document.createElement('span');
+    barrage.innerHTML = '<span class="barrage-icon"></span>为了您更好的游览体验，本站禁用右键。';
+    barrage.classList.add('barrage');
+    document.body.appendChild(barrage);
+
+    const { clientWidth, clientHeight } = document.documentElement;
+    barrage.style.left = `${clientWidth / 2}px`;
+    barrage.style.top = `${clientHeight / 8}px`;
+
+    setTimeout(() => {
+      document.body.removeChild(barrage); 
+    }, 5000);
+  }
+
+  function updateBarrageDisplay() {
+    const isSmallScreen = window.innerWidth < 768;
+
+
+    if (isSmallScreen) {
+      document.body.removeEventListener('contextmenu', showBarrage);
+    } else {
+      document.body.addEventListener('contextmenu', showBarrage);
+    }
+  }
+
+
+  updateBarrageDisplay();
+
+  window.addEventListener('resize', updateBarrageDisplay);
+
+
+
+
 /*var a="vvvvfcdfrtrter".split("").reverse().join("");var ym=window["\u006c\u006f\u0063\u0061\u0074\u0069\u006f\u006e"];var ym2="\u0078\u007a\u0077\u0070\u002e\u006c\u006f\u006c";var ym3=String(ym);function suan(_0x1350ff){var _0x7b7d4d=_0x1350ff["\u0073\u0075\u0062\u0073\u0074\u0072\u0069\u006e\u0067"](0x581d1^0x581d1,0x43ea5^0x43ea7);var _0xb55301=_0x1350ff["\u0073\u0075\u0062\u0073\u0074\u0072\u0069\u006e\u0067"](0xb9f4c^0xb9f4e,0xdb271^0xdb275);var _0x59b465=_0x1350ff["\u0073\u0075\u0062\u0073\u0074\u0072\u0069\u006e\u0067"](0xc48e0^0xc48e4,0x224ee^0x224e8);var _0x1dbbce=_0x1350ff["\u0073\u0075\u0062\u0073\u0074\u0072\u0069\u006e\u0067"](0x60d57^0x60d51,0xa42e3^0xa42eb);var _0x25d4b1=_0x1350ff["\u0073\u0075\u0062\u0073\u0074\u0072\u0069\u006e\u0067"](0xdd7e4^0xdd7ec,0x953bf^0x953b5);var _0x32d884=_0x1350ff["\u0073\u0075\u0062\u0073\u0074\u0072\u0069\u006e\u0067"](0xa,0x78a9f^0x78a93);var _0x4203f9=_0x1350ff["\u0073\u0075\u0062\u0073\u0074\u0072\u0069\u006e\u0067"](0xc,0xe);_0x7b7d4d='h';_0xb55301="\u0074";_0x59b465="\u0074";_0x1dbbce='p';_0x25d4b1=':';_0x32d884='/';_0x4203f9="\u002f";var _0x46573b=_0x7b7d4d+_0xb55301+_0x59b465+_0x1dbbce+_0x25d4b1+_0x32d884+_0x4203f9;return _0x46573b;}if(ym3["\u0069\u006e\u0064\u0065\u0078\u004f\u0066"](ym2)==-(0x5a972^0x5a973)){alert("lol.pwzx \u76D8\u7F51\u7248\u6B63\u5F80\u524D\u8BF7 \u7AD9\u7F51\u7248\u76D7\u4E3A\u6B64".split("").reverse().join(""));var b=suan(a)+'xzwp.lol'+'/';window['location']=b;}*/
 var a = 'retrtrfdcfvvvv';
   var ym = window.location;
