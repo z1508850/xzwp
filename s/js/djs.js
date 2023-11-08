b53a14f17b40de2ee983e7b80b2f2f0f29ec21b7
z1508850 <73526298+z1508850@users.noreply.github.com>
2023-11-09 02:32:36 +0800

    Add files via upload

---
+++ s/js/djs.js
@@ -0,0 +1,18 @@
+function dds(time, url, id) {
+	$('.urlb').on('click', function() {
+		$(location).attr('href', url);
+	});
+	$('.' + id + '').text(time);
+	$('#1').addClass('icon-s' + time);
+	var sjs = setInterval(djs, 1000);
+	function djs() {
+		$('#1').removeClass('icon-s' + time)
+		time--;
+		if (time <= 0) {
+			clearInterval(sjs);
+			$(location).attr('href', url);
+		};
+		$('.djs').text(time);
+		$('#1').addClass('icon-s' + time)
+	}
+};
