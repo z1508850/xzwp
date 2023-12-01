6997bbb3fddf0bcf8075dd294dfb6c7c79b059b7
z1508850 <73526298+z1508850@users.noreply.github.com>
2023-12-01 14:04:35 +0800

    Update time.js

--- js/time.js
+++ js/time.js
@@ -12,7 +12,7 @@ function siteTime(){
         var todayHour = today.getHours();
         var todayMinute = today.getMinutes();
         var todaySecond = today.getSeconds();
-        var t1 = Date.UTC(2023,11,28,14,59,00);
+        var t1 = Date.UTC(2023,12,01,14,04,00);
         var t2 = Date.UTC(todayYear,todayMonth,todayDate,todayHour,todayMinute,todaySecond);
         var diff = t2-t1;
         var diffYears = Math.floor(diff/years);
